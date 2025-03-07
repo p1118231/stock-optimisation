b887c7870cea85543aff2abfa9f886b9075ec70b
TRAVOR SSETTUBA <p1118231@tees.ac.uk>
2025-03-07 12:49:11 +0000

    Initial Gunicorn app deploy

---
+++ stock_optimisation_app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the model
+model_path = 'stock_optimization_model_gradient.pkl'
+model = joblib.load(model_path)
+
+# Example DataFrame loaded here for demonstration; in production, this might come from a database.
+df_products = pd.read_csv('/Users/breezy/EnhancedProductData.csv')
+
+@app.route('/predict/<int:product_id>', methods=['GET'])
+def predict(product_id):
+    try:
+        # Fetch the product details by ID
+        product = df_products[df_products['ProductId'] == product_id]
+        if product.empty:
+            return jsonify({'error': 'Product not found'}), 404
+
+        # Assume features are in the correct order
+        features = product[['Price', 'CompetitorPrice', 'CostOfGoods', 'SeasonalInfluence', 'MarketingSpend', 'CustomerDemographics','LeadTime', 'ReorderPoint', 'MinimumOrderQuantity', 'HoldingCosts', 'OrderingCosts', 'DemandVariability']].values
+        prediction = model.predict(features)
+        return jsonify({'product_id': product_id, 'predicted_stock_level': prediction[0]})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
